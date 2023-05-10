155713760a29d9979ce57faced1e60a362caa0f9
Gileade Kelvin <gileadekelvin@gmail.com>
2023-05-09 23:42:19 -0300

    feat: add marketing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+
 export const runtime = 'edge';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-5xl font-medium'>PlainNote</h1>
-    </main>
+    <>
+      <section className='space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-64'>
+        <div className='container flex max-w-[64rem] flex-col items-center gap-4 text-center'>
+          <Link
+            href='https://twitter.com/gileadekelvin'
+            className='rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium'
+            target='_blank'
+          >
+            Follow along on Twitter
+          </Link>
+          <h1 className='text-3xl font-bold sm:text-5xl md:text-6xl lg:text-7xl'>Plain Note</h1>
+          <p className='max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8'>
+            Bring simplicity back to note-taking and unleash your creativity. Just write simple
+            notes and share knowledge!
+          </p>
+          <div className='space-x-4'>
+            <Link href='/' className={cn(buttonVariants({ size: 'lg' }))}>
+              Get Started
+            </Link>
+            <Link
+              href='https://github.com/gileadekelvin/plain-note'
+              target='_blank'
+              rel='noreferrer'
+              className={cn(buttonVariants({ variant: 'outline', size: 'lg' }))}
+            >
+              GitHub
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
