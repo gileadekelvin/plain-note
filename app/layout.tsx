f4392c6fad67630c4ae8a721ad1daa535b84f0f7
Gileade Kelvin <gileadekelvin@gmail.com>
2023-05-10 00:03:43 -0300

    feat: add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <Navbar />
         {children}
         <Analytics />
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export const runtime = 'edge';
 export default function Home() {
   return (
     <>
-      <section className='space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-48'>
+      <section className='space-y-6 pb-8 pt-12 md:pb-12 md:pt-10 lg:py-36'>
         <div className='container flex max-w-[64rem] flex-col items-center gap-4 text-center'>
           <Link
             href='https://twitter.com/gileadekelvin'
